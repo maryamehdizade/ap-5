e2ffb616ba1ce4d1bb02c5364bf07630cc9fcae7
unknown <maryam.mehdizadeh1384@gmail.com>
2024-06-25 13:27:00 +0330

    server frame

--- srever/src/main/java/gui/Server.java
+++ srever/src/main/java/gui/Server.java
@@ -1,4 +1,21 @@
 package gui;
 
+import javax.swing.*;
+
 public class Server {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("server");
+        jFrame.setSize(450, 450);
+        jFrame.setLayout(new BoxLayout(jFrame.getContentPane(), BoxLayout.Y_AXIS));
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
+
+        JScrollPane scrollPane = new JScrollPane(jPanel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        jFrame.add(scrollPane);
+        jFrame.setVisible(true);
+    }
 }
